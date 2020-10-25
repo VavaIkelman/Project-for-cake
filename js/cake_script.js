63c114fc5e372dc9bd10c518b0437395302558ed
VavaIkelman <mr.ilkevich@gmai.com>
2020-10-25 17:00:36 +0300

    Добавлены правки для Бургер-меню

--- js/cake_script.js
+++ js/cake_script.js
@@ -1,4 +1,15 @@
-let  admin , name;
-name = "John";
-admin = name;
-alert(admin);
+let header__burger = document.querySelector('.header__burger');
+let nav_header = document.querySelector('.nav_header');
+let back = document.querySelector('body');
+let header_menu_list = document.querySelector('.header_menu_list');
+
+header__burger.onclick = function(){
+    header__burger.classList.toggle('active');
+    nav_header.classList.toggle('active');
+    back.classList.toggle('lock');
+}
+
+header_menu_list.onclick = function () {
+    header_menu_list.classList.remove('active');
+    back.classList.toggle('lock');
+}
